63a4c0368b1f890d3ee7ff064ad847843b54cc2a
INWOONG OH <mn00149@naver.com>
2022-05-24 23:45:18 +0900

    알고리즘 풀이

--- time.py
+++ time.py
@@ -11,3 +11,21 @@ def time(n):
 
 
 time(5)
+
+
+def nightKing(input):
+    dx, dy = [1, -1, 2, 2, -2, -2, 1, -1], [2, 2, 1, -1, 1, -1, -2, -2]
+    trans = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+    x = int(input[1])
+    y = trans[input[0]]
+    count = 0
+    for i in range(0, 8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if(nx >= 1 and nx <= 8 and ny >= 1 and ny <= 8):
+            count += 1
+        nx, ny = 0, 0
+    print(count)
+
+
+nightKing("a1")

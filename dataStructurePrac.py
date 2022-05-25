ede9d2c3258d2b11aad643153a9865e4c4cc5a86
INWOONG OH <mn00149@naver.com>
2022-05-25 23:48:23 +0900

    dataStructure

--- dataStructurePrac.py
+++ dataStructurePrac.py
@@ -1,3 +1,5 @@
+# Stack
+from collections import deque
 stack = []
 # 5삽입 6삽입 7삽입 삭제 9삽입 6삽입 삭제
 stack.append(5)
@@ -7,5 +9,18 @@ stack.pop()
 stack.append(9)
 stack.append(6)
 stack.pop()
-print(stack)
-print(stack[::-1])
+print(stack)  # 최하단부터 출력
+print(stack[::-1])  # 최상단부터 출력
+# Queue
+queue = deque()
+queue.append(1)
+queue.append(3)
+queue.append(5)
+queue.append(7)
+queue.append(2)
+queue.popleft()
+queue.append(1)
+queue.popleft()
+queue.popleft()
+queue.reverse()
+print(queue)
